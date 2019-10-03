3f233d2171968576d187332538b40ababcf732a7
Cesar Jimenez <cijgr@hotmail.com>
2019-10-03 15:29:38 -0500

    Part3a, exercise 3.3 step3 COMPLETED

--- index.js
+++ index.js
@@ -31,18 +31,33 @@ let persons = [
 
 const now = new Date();
 
+// Fetch home
 app.get("/", (req, res) => {
   res.send("<h1>Hello!</h1>");
 });
 
+// Fetch list of all persons
 app.get("/api/persons", (req, res) => {
   res.json(persons);
 });
 
+// Fetch info
 app.get("/info", (req, res) => {
   res.send(
     `<p>Phonebook has info for ${persons.length} people.</p><p>${now}</p>`
   );
+});
+
+// Fetch single resources
+app.get("/persons/:id", (req, res) => {
+  const id = Number(req.params.id);
+  const person = persons.find(p => p.id === id);
+
+  if (person) {
+    res.json(person);
+  } else {
+    res.status(404).end();
+  }
 });
 
 const PORT = 3001;
